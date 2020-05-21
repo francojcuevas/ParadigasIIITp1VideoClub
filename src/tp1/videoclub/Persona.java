e2c3833a4c9af627ba0fe8e241baeb06080c0834
francojcuevas <franco.j.cuevas@gmail.com>
2020-05-21 00:08:11 -0300

    Clases Persona y Usuario creadas

---
+++ src/tp1/videoclub/Persona.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1.videoclub;
+
+/**
+ *
+ * @author Chuck
+ */
+public class Persona {
+
+    public String Nombre;
+    public int ID;
+    
+    public Persona(String Nombre, int ID) {
+        this.Nombre = Nombre;
+        this.ID = ID;
+    }
+   
+    public void alquilarPelicula(){
+       // Codigo para alquilar pelicula;
+    }
+    
+}
---
+++ src/tp1/videoclub/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1.videoclub;
+
+/**
+ *
+ * @author Chuck
+ */
+public class Usuario extends Persona{
+    
+    public Usuario (String Nombre, int ID){
+        
+        super(Nombre, ID);
+        this.Nombre = Nombre;
+        this.ID = ID;
+    }   
+ 
+ 
+}
